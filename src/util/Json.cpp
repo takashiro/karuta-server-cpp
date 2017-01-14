b92cc407d8137c6495d30ad4069054ada1d5f014
Takashiro <takashiro@qq.com>
2017-01-14 18:39:18 +0800

    Add Json

---
+++ src/util/Json.cpp
@@ -0,0 +1,451 @@
+#include "Json.h"
+
+#include <sstream>
+#include <fstream>
+
+BA_NAMESPACE_BEGIN
+
+Json::Json()
+    : m_type(Null)
+{
+}
+
+Json::Json(bool value)
+    : m_type(Boolean)
+{
+    m_value.boolean = value;
+}
+
+Json::Json(int value)
+    : m_type(Number)
+{
+    m_value.number = value;
+}
+
+Json::Json(double value)
+    : m_type(Number)
+{
+    m_value.number = value;
+}
+
+Json::Json(const char *value)
+{
+    m_type = String;
+    m_value.str = new std::string(value);
+}
+
+Json::Json(const std::string &value)
+    : m_type(String)
+{
+    m_value.str = new std::string(value);
+}
+
+Json::Json(std::string &&value)
+    : m_type(String)
+{
+    m_value.str = new std::string(std::move(value));
+}
+
+Json::Json(const JsonArray &array)
+    : m_type(Array)
+{
+    m_value.array = new JsonArray(array);
+}
+
+Json::Json(JsonArray &&array)
+    : m_type(Array)
+{
+    m_value.array = new JsonArray(std::move(array));
+}
+
+Json::Json(const JsonObject &object)
+    : m_type(Object)
+{
+    m_value.object = new JsonObject(object);
+}
+
+Json::Json(JsonObject &&object)
+    : m_type(Object)
+{
+    m_value.object = new JsonObject(std::move(object));
+}
+
+Json::Json(const Json &source)
+{
+    copy(source);
+}
+
+Json::Json(Json &&source)
+    : m_type(source.m_type)
+    , m_value(source.m_value)
+{
+    source.m_type = Null;
+}
+
+Json::~Json()
+{
+    release();
+}
+
+bool Json::toBool() const
+{
+    switch (m_type) {
+	case Boolean:
+        return m_value.boolean;
+	case Number:
+        return (int) m_value.number != 0;
+	case String:
+        return !m_value.str->empty();
+	case Array:
+        return m_value.array->size() > 0;
+	case Object:
+        return m_value.object->size() > 0;
+	default:
+		return false;
+	}
+}
+
+double Json::toDouble() const
+{
+    if (m_type == Number)
+        return m_value.number;
+
+    if (m_type == String) {
+		std::stringstream s;
+		s << *(m_value.str);
+		double number;
+		s >> number;
+		return number;
+	}
+
+	return 0.0;
+}
+
+int Json::toInt() const
+{
+    if (m_type == Number)
+        return static_cast<int>(m_value.number);
+
+    if (m_type == String) {
+		std::stringstream s;
+		s << *(m_value.str);
+		int number;
+		s >> number;
+		return number;
+	}
+
+    return 0;
+}
+
+std::string Json::toString() const
+{
+    if (m_type == String)
+        return *(m_value.str);
+
+    if (m_type == Number) {
+        std::stringstream s;
+        s << m_value.number;
+        std::string str;
+        s >> str;
+        return str;
+    }
+
+    return std::string();
+}
+
+JsonArray &Json::array()
+{
+    return *(m_value.array);
+}
+
+const JsonArray &Json::toArray() const
+{
+    return *(m_value.array);
+}
+
+JsonObject &Json::object()
+{
+    return *(m_value.object);
+}
+
+const JsonObject &Json::toObject() const
+{
+    return *(m_value.object);
+}
+
+Json &Json::operator =(const Json &source)
+{
+    release();
+    copy(source);
+    return *this;
+}
+
+Json &Json::operator = (Json &&source)
+{
+    release();
+    m_type = source.m_type;
+    m_value = source.m_value;
+    source.m_type = Null;
+    return *this;
+}
+
+const Json &Json::value(const std::string &key, const Json &defaultValue) const
+{
+	if (contains(key))
+		return value(key);
+	return defaultValue;
+}
+
+size_t Json::size() const
+{
+	if (isArray())
+        return m_value.array->size();
+	else if (isObject())
+        return m_value.object->size();
+	return 0;
+}
+
+void Json::clear()
+{
+    if (m_type == Array) {
+        m_value.array->clear();
+    } else if (m_type == Object) {
+        m_value.object->clear();
+    } else if (m_type == String) {
+        m_value.str->clear();
+    } else if (m_type == Number) {
+        m_value.number = 0.0;
+    }
+}
+
+void Json::release()
+{
+    if (m_type == Array) {
+        delete m_value.array;
+    } else if (m_type == Object) {
+        delete m_value.object;
+    } else if (m_type == String) {
+        delete m_value.str;
+    }
+}
+
+namespace
+{
+
+bool json_try_read(std::istream &in, char ch)
+{
+    char first;
+    do {
+        in.get(first);
+    } while (isspace(first));
+
+    if (first == ch)
+        return true;
+
+    in.unget();
+    return false;
+}
+
+bool json_try_read(std::istream &in, const char *str)
+{
+    char first;
+    do {
+        in.get(first);
+    } while (isspace(first));
+
+    const char *cur = str;
+    if (*cur != first) {
+        in.unget();
+        return false;
+    }
+
+    cur++;
+    while (*cur) {
+        if (json_try_read(in, *cur)) {
+            cur++;
+        } else {
+            while (cur != str) {
+                cur--;
+                in.putback(*cur);
+            }
+            return false;
+        }
+    }
+    return true;
+}
+
+}
+
+std::istream &operator>>(std::istream &in, Json &value)
+{
+    if (value.type() != Json::Null)
+        return in;
+
+    if (json_try_read(in, "null")) {
+        value.m_type = Json::Null;
+    } else if (json_try_read(in, "false")) {
+        value.m_type = Json::Boolean;
+        value.m_value.boolean = false;
+    } else if (json_try_read(in, "true")) {
+        value.m_type = Json::Boolean;
+        value.m_value.boolean = true;
+    } else if (json_try_read(in, '"')) {
+        value.m_type = Json::String;
+        std::string str;
+        char ch;
+        while (!in.eof()) {
+            in.get(ch);
+            if (ch == '\\') {
+                in.get(ch);
+                if (ch == '"') {
+                    str += ch;
+                    continue;
+                }
+            } else if (ch == '"') {
+                break;
+            }
+            str += ch;
+        }
+        value.m_value.str = new std::string(std::move(str));
+    } else {
+        char ch;
+        in.get(ch);
+        if (ch == '-' || (ch >= '0' && ch <= '9') || ch == '.') {
+            in.unget();
+            value.m_type = Json::Number;
+            in >> value.m_value.number;
+        } else if (ch == '[') {
+            value.m_type = Json::Array;
+            value.m_value.array = new JsonArray();
+            if (json_try_read(in, ']')) {
+                return in;
+            }
+
+            JsonArray &array = (*value.m_value.array);
+            while (true) {
+                Json element;
+                in >> element;
+                array.push_back(std::move(element));
+
+                if (!json_try_read(in, ',')) {
+                    if (!(json_try_read(in, ']'))) {
+                        //vError("Expect ]");
+                    } else {
+                        break;
+                    }
+                }
+            }
+        } else if (ch == '{') {
+            value.m_type = Json::Object;
+            value.m_value.object = new JsonObject();
+            if (json_try_read(in, '}')) {
+                return in;
+            }
+
+            JsonObject &object = *(value.m_value.object);
+            while (true) {
+                Json key;
+                in >> key;
+                if (key.type() != Json::String) {
+                    //vError("Expect JSON key string.");
+                    break;
+                }
+                if (!json_try_read(in, ':')) {
+                    //vError("Expect :");
+                    break;
+                }
+
+                Json value;
+                in >> value;
+                object[key.toString()] = std::move(value);
+
+                if (!json_try_read(in, ',')) {
+                    if (!(json_try_read(in, '}'))) {
+                        //vError("Expect }");
+                    } else {
+                        break;
+                    }
+                }
+            }
+        } else {
+            //vError("Unexpected character: " << ch);
+        }
+    }
+    return in;
+}
+
+std::ostream &operator<<(std::ostream &out, const Json &value)
+{
+    switch (value.type()) {
+    case Json::Null:
+        out << "null";
+        break;
+    case Json::Boolean:
+        if (value.toBool())
+            out << "true";
+        else
+            out << "false";
+        break;
+    case Json::Number:
+        out << value.toDouble();
+        break;
+    case Json::String:
+        out << '"' << value.toString() << '"';
+        break;
+    case Json::Array:{
+        out << '[';
+        const JsonArray &elements = value.toArray();
+        size_t size = elements.size();
+        if (size > 0) {
+            out << elements.at(0);
+            for(size_t i = 1; i < size; i++)
+                out << ", " << elements.at(i);
+        }
+        out << ']';
+        break;
+    }
+    case Json::Object:{
+        out << '{';
+        const JsonObject &jsonObject = value.toObject();
+        if (!jsonObject.empty()) {
+            JsonObject::const_iterator iter = jsonObject.begin();
+            out << '"' << iter->first << "\" : " << iter->second;
+            for (iter++; iter != jsonObject.end(); iter++) {
+                out << ", \"" << iter->first << "\" : " << iter->second;
+            }
+        }
+        out << '}';
+        break;
+    }
+    default:
+        //vError("invalid JSON value can't be printed");
+        break;
+    }
+    return out;
+}
+
+void Json::copy(const Json &source)
+{
+    m_type = source.type();
+    switch (m_type) {
+    case Json::Boolean:
+        m_value.boolean = source.m_value.boolean;
+        break;
+    case Json::Number:
+        m_value.number = source.m_value.number;
+        break;
+    case Json::String:
+        m_value.str = new std::string(*source.m_value.str);
+        break;
+    case Json::Array:
+        m_value.array = new JsonArray(*source.m_value.array);
+        break;
+    case Json::Object:
+        m_value.object = new JsonObject(*source.m_value.object);
+        break;
+    default:
+        break;
+    }
+}
+
+BA_NAMESPACE_END
---
+++ src/util/Json.h
@@ -0,0 +1,120 @@
+#pragma once
+
+#include "global.h"
+
+#include <istream>
+#include <ostream>
+#include <string>
+#include <vector>
+#include <map>
+
+BA_NAMESPACE_BEGIN
+
+class Json;
+typedef std::vector<Json> JsonArray;
+typedef std::map<std::string, Json> JsonObject;
+
+class Json
+{
+public:
+	enum Type
+	{
+        Null,
+		Boolean,
+		Number,
+		String,
+		Array,
+		Object
+	};
+
+    //Null
+	Json();
+
+    //Boolean
+    Json(bool value);
+
+    //Number
+    Json(int value);
+    Json(double value);
+
+    //String
+    Json(const char *value);
+	Json(const std::string &value);
+    Json(std::string &&value);
+
+    //Array
+    Json(const JsonArray &array);
+    Json(JsonArray &&array);
+
+	//Object
+    Json(const JsonObject &object);
+    Json(JsonObject &&object);
+
+    Json(const Json &source);
+    Json(Json &&source);
+    ~Json();
+
+    Type type() const { return m_type; }
+	bool isNull() const { return m_type == Null; }
+	bool isNumber() const { return m_type == Number; }
+	bool isString() const { return m_type == String; }
+	bool isArray() const { return m_type == Array; }
+	bool isObject() const { return m_type == Object; }
+
+	bool toBool() const;
+
+    double toDouble() const;
+	int toInt() const;
+
+    std::string toString() const;
+
+    JsonArray &array();
+    const JsonArray &toArray() const;
+
+    JsonObject &object();
+    const JsonObject &toObject() const;
+
+    //Assignment functions
+    Json &operator =(const Json &source);
+    Json &operator =(Json &&source);
+
+	//Array Functions
+    Json &operator[](uint i) { return (*m_value.array)[i]; }
+
+    const Json &operator[](uint i) const { return m_value.array->at(i); }
+    const Json &at(uint i) const { return m_value.array->at(i); }
+
+	//Object Functions
+    Json &operator[](const std::string &key) { return (*(m_value.object))[key]; }
+
+    const Json &operator[](const std::string &key) const { return m_value.object->at(key); }
+    const Json &value(const std::string &key) const { return m_value.object->at(key); }
+    const Json &value(const std::string &key, const Json &defaultValue) const;
+
+    bool contains(const std::string &key) const { return m_value.object->find(key) != m_value.object->end(); }
+
+	//Array/Object functions
+    size_t size() const;
+	void clear();
+
+    friend std::istream &operator>>(std::istream &in, Json &value);
+    friend std::ostream &operator<<(std::ostream &out, const Json &value);
+
+private:
+    void copy(const Json &source);
+    void release();
+
+    Json::Type m_type;
+
+    union Value
+    {
+        bool boolean;
+        double number;
+        std::string *str;
+        JsonArray *array;
+        JsonObject *object;
+    };
+    Value m_value;
+};
+
+BA_NAMESPACE_END
